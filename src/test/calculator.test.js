a2099bd9a9e158660a426c3398252c70431f3559
pozafly <pozafly@gmail.com>
2022-10-09 22:30:43 +0900

    add test calculator

---
+++ src/test/calculator.test.js
@@ -0,0 +1,23 @@
+const Calculator = require('../calculator.js');
+
+describe('Calculator', () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it('inits with 0', () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it('sets', () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+
+  it('clear', () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+});
